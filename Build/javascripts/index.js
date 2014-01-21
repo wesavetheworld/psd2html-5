2cf3835e9d0d316629937e9f07b4fd7006ba741b
scriptfans <chen.ruijie@gmail.com>
2014-01-21 17:22:38 +0800

    实现图片轮播

--- Build/javascripts/index.js
+++ Build/javascripts/index.js
@@ -1,34 +1,43 @@
 (function() {
   $(function() {
-    var carousel;
+    var carousel, items;
     $('#nav>li').each(function() {
       var li, timer;
       li = $(this);
       timer = null;
       li.on('mouseenter', function() {
-        if (timer) {
-          clearTimeout(timer);
-        }
-        return li.find('ul').show();
+        clearTimeout(timer);
+        li.addClass('active');
+        return li.find('ul').slideDown('fast');
       });
       return li.on('mouseleave', function() {
-        if (timer) {
-          clearTimeout(timer);
-        }
+        clearTimeout(timer);
+        li.removeClass('active');
         return timer = setTimeout((function() {
-          return li.find('ul').hide();
+          return li.find('ul').slideUp('fast');
         }), 20);
       });
     });
-    carousel = $('#carousel');
-    return carousel.find('li').each(function(idx) {
+    carousel = $('.jcarousel');
+    items = carousel.find('a');
+    carousel.jcarousel({
+      wrap: 'circular'
+    }).jcarouselAutoscroll({
+      interval: 3000,
+      target: '+=1',
+      autostart: true
+    });
+    carousel.on('jcarousel:scrollend', function(event, widget) {
+      var idx;
+      idx = items.index(widget.target());
+      carousel.find('li.active').removeClass('active');
+      return carousel.find('li').eq(idx).addClass('active');
+    });
+    return $('.jcarousel li').each(function(idx) {
       var li;
       li = $(this);
-      return li.on('click', function(e) {
-        li.addClass('active').siblings().removeClass('active');
-        return carousel.find('a').each(function(i) {
-          return $(this)[i === idx ? 'show' : 'hide']();
-        });
+      return li.click(function() {
+        return carousel.jcarousel('scroll', idx);
       });
     });
   });
--- javascripts/index.coffee
+++ javascripts/index.coffee
@@ -3,19 +3,31 @@ $ ->
     li = $(@)
     timer = null
     li.on 'mouseenter', ->
-      clearTimeout timer if timer
-      li.find('ul').show();
+      clearTimeout timer
+      li.addClass 'active'
+      li.find('ul').slideDown('fast');
     li.on 'mouseleave', ->
-      clearTimeout timer if timer
-      timer = setTimeout (-> li.find('ul').hide()), 20
+      clearTimeout timer
+      li.removeClass 'active'
+      timer = setTimeout (-> li.find('ul').slideUp('fast')), 20
 
-  carousel = $('#carousel')
-  carousel.find('li').each (idx) ->
-    li = $(@)
-    li.on 'click', (e) ->
-      li.addClass('active').siblings().removeClass 'active'
-      carousel.find('a').each (i) ->
-        $(@)[if(i == idx ) then 'show' else 'hide']()
+  carousel = $('.jcarousel')
+  items = carousel.find('a')
+
+  carousel.jcarousel(wrap: 'circular').jcarouselAutoscroll
+    interval: 3000
+    target: '+=1'
+    autostart: true
+
+  carousel.on 'jcarousel:scrollend', (event, widget) ->
+    idx = items.index widget.target()
+    carousel.find('li.active').removeClass 'active'
+    carousel.find('li').eq(idx).addClass 'active'
+
+  $('.jcarousel li').each (idx) ->
+    li = $ @
+    li.click ->
+      carousel.jcarousel('scroll', idx);
 
 
 
